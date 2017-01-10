9d68a08c67a392157a096083f653088995a73a5d
janko33bd <janko33bd@gmail.com>
2017-01-10 20:39:43 +0100

    more missed text, bws url change, insight url change, removing USD

--- src/js/controllers/tx-details.js
+++ src/js/controllers/tx-details.js
@@ -120,7 +120,7 @@ angular.module('copayApp.controllers').controller('txDetailsController', functio
 
   $scope.viewOnBlockchain = function() {
     var btx = $scope.btx;
-    var url = 'https://' + ($scope.getShortNetworkName() == 'test' ? 'test-' : '') + 'insight.bitpay.com/tx/' + btx.txid;
+    var url = 'https://' + ($scope.getShortNetworkName() == 'test' ? 'test-' : '') + 'node.blackcoin.io/insight/tx/' + btx.txid;
     var optIn = true;
     var title = null;
     var message = gettextCatalog.getString('View Transaction on Insight');
